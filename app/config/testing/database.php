da778ac6d3d97605216899482c2a4b6a3d295b0f
Anderson Mota <anderson.mota@lqdi.net>
2014-07-24 20:00:51 -0300

    Preparado e configurado o ambiente para fazer os testes.

--- app/config/testing/database.php
+++ app/config/testing/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'homestead',
+			'database'  => 'tests',
 			'username'  => 'homestead',
 			'password'  => 'secret',
 			'charset'   => 'utf8',
@@ -66,7 +66,7 @@ return array(
 		'pgsql' => array(
 			'driver'   => 'pgsql',
 			'host'     => 'localhost',
-			'database' => 'homestead',
+			'database' => 'tests',
 			'username' => 'homestead',
 			'password' => 'secret',
 			'charset'  => 'utf8',
